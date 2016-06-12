ee8f4162fde44c926e044b29f8d9dfcd05bb6294
Matthew Einhorn <moiein2000@gmail.com>
2016-06-12 00:52:54 -0400

    Update docs.

--- pybarst/__init__.py
+++ pybarst/__init__.py
@@ -17,11 +17,19 @@ import sys
 import os
 from os.path import join, isdir
 
+__all__ = ('dep_bins', )
+
 __version__ = '2.1-dev'
 
 __min_barst_version__ = 20000
 
 dep_bins = []
+'''A list of paths to the binaries used by the library. It can be used during
+packaging for including required binaries.
+
+It is read only.
+'''
+
 _pybarst = join(sys.prefix, 'share', 'pybarst', 'bin')
 if isdir(_pybarst):
     os.environ["PATH"] += os.pathsep + _pybarst
